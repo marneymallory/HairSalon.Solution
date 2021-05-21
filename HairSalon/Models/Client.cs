0025a4fe449556f4c57833bab8412cdbdcb7112a
marneymallory <79782193+marneymallory@users.noreply.github.com>
2021-05-21 09:54:48 -0800

    add logic to Client.cs file

---
+++ HairSalon/Models/Client.cs
@@ -0,0 +1,12 @@
+namespace HairSalon.Models
+{
+  public class Client
+  {
+    public int ClientId { get; set; }
+    public string Name { get; set; }
+    public string Number { get; set; }
+    public string Preferences { get; set; }
+    public int StylistId { get; set; }
+    public virtual Stylist Stylist { get; set; }
+  }
+}
